bec07a5d7bc2070ecad9928a4c13ad20682f3335
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-19 10:24:22 -0700

    change using directive to HairSalon.Models from Template.Models

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Template.Models;
+using HairSalon.Models;
 
 namespace HairSalon
 {
