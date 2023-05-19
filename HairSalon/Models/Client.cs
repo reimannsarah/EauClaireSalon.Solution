ec5ae4eaf4e4c8357b6fb8df29db8d98ca9c6947
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-19 14:24:30 -0700

    update Id to ClientId

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public int Id { get; set; }
+    public int ClientId { get; set; }
     public string Name { get; set; }
     public Stylist Stylist { get; set; }
     public int StylistId { get; set; }
