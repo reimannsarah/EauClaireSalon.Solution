7d70874f999074ef089dd31d94aaba85b7cccde4
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-19 10:25:59 -0700

    add route to Index, Create, and Details  pages

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,74 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists
+                                  .Include(stylist => stylist.Clients)
+                                  .FirstOrDefault(stylist => stylist.Id == id);
+      return View(thisStylist);
+    }
+
+//     public ActionResult Edit(int id)
+//     {
+//       // Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+//       return View(thisCategory);
+//     }
+
+//     [HttpPost]
+//     public ActionResult Edit(Category category)
+//     {
+//       // _db.Categories.Update(category);
+//       // _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
+
+//     public ActionResult Delete(int id)
+//     {
+//       // Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+//       return View(thisCategory);
+//     }
+
+//     [HttpPost, ActionName("Delete")]
+//     public ActionResult DeleteConfirmed(int id)
+//     {
+//       // Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+//       // _db.Categories.Remove(thisCategory);
+//       // _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
+  }
+}
